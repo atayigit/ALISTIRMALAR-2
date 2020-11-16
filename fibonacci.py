3dbaca290e2bdfab9cb124505892f1735dabb7d1
atayigit <64311652+atayigit@users.noreply.github.com>
2020-11-16 09:36:35 +0300

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -9,9 +9,9 @@ def fibonacci(n):
         return fibonacci(n-1) + fibonacci(n-2)
         
         
-n=int (input(("Kaça kadar?")))
+n=int (input(("Kaça elemanlı?")))
 liste=[]
-for i in range(1,n):
+for i in range(1,n+1):
         liste.append(fibonacci(i))
 
 
