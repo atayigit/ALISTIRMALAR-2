407fc26df1198bc387d96ad71578976175676c5f
atayigit <64311652+atayigit@users.noreply.github.com>
2020-11-18 15:00:51 +0300

    Add files via upload

---
+++ fibonacci.py
@@ -0,0 +1,24 @@
+
+def fibonacci(n):
+    liste=[]
+    if n <= 1:
+        return 1
+        liste.append(1)
+        
+    
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
+        
+        
+n=int (input(("Kaça elemanlı?")))
+liste=[]
+#fiboacci serisi ilk iki tane 1 le başlar bunu ekrana yansıtmak için
+#listeye 1 sayısını ekledim
+for i in range(0,n):
+        liste.append(fibonacci(i))
+
+
+print(*liste,sep=" ")
+    
+    
+    
